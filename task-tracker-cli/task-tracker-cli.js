772cc8dc7e96a6dfc1d661d1098ab69a0ea75d0d
Felix Alexandre <felixnovela104@gmail.com>
2026-09-07 02:09:53 +0200

    Implement delete and update functions, using an auxiliary function to find a specific task

--- task-tracker-cli/task-tracker-cli.js
+++ task-tracker-cli/task-tracker-cli.js
@@ -3,19 +3,29 @@ const taskList = []
 
 const generateId = () => {
     let currentId = 1
-    if(taskList.length > 0){
+    if (taskList.length > 0) {
         currentId = taskList.at(-1).id + 1
         return currentId
     }
     return currentId
-    
+
 }
 
-
+const findTask = (taskId) => {
+    if (taskList.length > 0) {
+        for (let i = 0; i < taskList.length; i++) {
+            if (taskList[i].id === taskId) {
+                return i
+                
+            }
+        } 
+    }
+    return -1
+}
 
 const add = (taskDescription) => {
     task = {
-        id:  generateId(),
+        id: generateId(),
         description: taskDescription,
         status: "todo",
         createdAt: new Date(),
@@ -24,13 +34,27 @@ const add = (taskDescription) => {
     taskList.push(task)
 }
 
+const deleteTask = (taskId) => {
+    let index = findTask(taskId)
+    if (index !== -1) {
+        taskList.splice(index,1)
+    }
+}
+
+const updateTask = (taskId, newTaskDescription) => {
+    let index = findTask(taskId)
+    if (index !== -1) {
+        taskList[index].description = newTaskDescription
+        taskList[index].updatedAt = new Date()
+    }
+}
 const list = () => {
     taskList.forEach(element => {
-        console.log(" id",element.id,"\n",
-            "description:",element.description,"\n",
-            "Status: ",element.status,"\n",
-            "Created At: ",element.createdAt,"\n",
-            "Created At: ",element.createdAt,"\n"
+        console.log(" id", element.id, "\n",
+            "description:", element.description, "\n",
+            "Status: ", element.status, "\n",
+            "Created At: ", element.createdAt, "\n",
+            "updatedAt At: ", element.updatedAt, "\n"
         )
     });
 }
@@ -39,5 +63,8 @@ add("Task 1")
 add("Task 2")
 
 add("Task 3")
+
+list()
+updateTask(3,"Task Atualizada")
 list()
 
